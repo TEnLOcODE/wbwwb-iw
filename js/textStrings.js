4ca506be8ada49d43b5f504d226b46fb871ecbe3
TEnLOcODE <87935299+TEnLOcODE@users.noreply.github.com>
2023-08-31 17:15:35 +0300

    Update textStrings.js

--- js/textStrings.js
+++ js/textStrings.js
@@ -3,93 +3,93 @@
 ///////////////////////////////////
 var textStrings_CN = {
      // Act 1
-      "niceHat"              : "אה-יוו אחלה כובע"                                          ,
-      "outtaHere"            : "אוקע יאללה עוף מפה"                                ,
-      "getARoom"             : "איכסהה!! לכו תשיגו חדר במלון!"                                  ,
-      "notCoolAnymore"       : "נעע.. כובעים זה כבר לא כזה מגניב"                          ,
-      "tvOnTv"               : "טלויזיה! ב.. טלויזיה!!"                                        ,
-      "cricky"               : "יווו צרצרון!!"                                        ,
-    "tooManyCrickets"      : "אוקיי עכשיו יש יותר מדי צרצרים"                         ,
-     "normalPeep"           : "נעע סתם איש רגיל"                                    ,
-     "normalPeeps"          : "אנשים רגילים סה"כ"                                ,
-     "wowNothing"           : "יוו, זה *כלום*"                                  ,
+      "niceHat"              : "\x80\x84-\x89\x85\x85 \x80\x87\x8C\x84 \x8B\x85\x81\x92"                                          ,
+      "outtaHere"            : "\x80\x85\x97\x92 \x89\x80\x8C\x8C\x84 \x92\x85\x93 \x8E\x94\x84"                                ,
+      "getARoom"             : "\x80\x89\x8B\x91\x84\x84!! \x8C\x8B\x85 \x9A\x99\x89\x82\x85 \x87\x83\x98 \x81\x8E\x8C\x85\x8F!"                                  ,
+      "notCoolAnymore"       : "\x90\x92\x92.. \x8B\x85\x81\x92\x89\x8D \x86\x84 \x8B\x81\x98 \x8C\x80 \x8B\x86\x84 \x8E\x82\x90\x89\x81"                          ,
+      "tvOnTv"               : "\x88\x8C\x85\x89\x86\x89\x84! \x81.. \x88\x8C\x85\x89\x86\x89\x84!!"                                        ,
+      "cricky"               : "\x89\x85\x85\x85 \x96\x98\x96\x98\x85\x8F!!"                                        ,
+    "tooManyCrickets"      : "\x80\x85\x97\x89\x89 \x92\x8B\x99\x89\x85 \x89\x99 \x89\x85\x9A\x98 \x8E\x83\x89 \x96\x98\x96\x98\x89\x8D"                         ,
+     "normalPeep"           : "\x90\x92\x92 \x91\x9A\x8D \x80\x89\x99 \x98\x82\x89\x8C"                                    ,
+     "normalPeeps"          : "\x80\x90\x99\x89\x8D \x98\x82\x89\x8C\x89\x8D \x91\x84"\x8B"                                ,
+     "wowNothing"           : "\x89\x85\x85, \x86\x84 *\x8B\x8C\x85\x8D*"                                  ,
      
      // Act 2
-     "crazySquareAttacks"   : "מרובע משוגע תוקף!"                                 ,
-     "justMissed"           : "באסהה, איזה פספוס"                                 ,
-    "somethingInteresting" : "(ת'צריך לתפוס'תם עושים *משהו* מעניין...)" ,
-     "whoIsScreaming"       : "(ת'צריך לראות מי צרח עליהם）"               ,
-    "circleFearsSquares"   : "עיגול מפחד ממרובע"                                  ,
-    "whoScaresThem"        : "(ת'צריך לראות גם *ממי* הם מפחדים)"         ,
-     "areTheyScared"        : "(ת'צריך לתפוס'תם *מפחדים* ממרובע)"       , 
-     "squaresSnubCircles"   : "מרובעים מדחיקים עיגולים!!"                                  ,
-    "areTheySnubbed"       : "(ת'צריך לתפוס'תם *תוך כדי* שהם מדחיקים עיגול)"        , 
-    "everyoneHates"        : "כולם שונאים את כולם!!1!"                           ,
-   "almostEveryoneHates"  : "כמעט כולם שונאים את כולם..."                     ,
-   "squaresHateCircles"   : "מרובעים שונאים עיגולים!!"                                  ,
-   "circlesHateSquares"   : "עיגולים שונאים מרובעים!!"                                  ,
-   "areTheyYelling"       : "(ת'צריך לתפוס'תם *צורחים* על אחרים)"              , 
-    "nerdsNow"             : "מה החננות האלה עושים עכשיו"                        ,
-   "schockedPeep"         : "למה האיש הזה בהלם?"                              ,
-     "whatever"             : "נעע.. שיהיה"                                              ,
+     "crazySquareAttacks"   : "\x8E\x98\x85\x81\x92 \x8E\x99\x85\x82\x92 \x9A\x85\x97\x93!"                                 ,
+     "justMissed"           : "\x81\x80\x91\x84\x84, \x80\x89\x86\x84 \x94\x91\x94\x85\x91"                                 ,
+    "somethingInteresting" : "(\x9A'\x96\x98\x89\x8A \x8C\x9A\x94\x85\x91'\x9A\x8D \x92\x85\x99\x89\x8D *\x8E\x99\x84\x85* \x8E\x92\x90\x89\x89\x8F...)" ,
+     "whoIsScreaming"       : "(\x9A'\x96\x98\x89\x8A \x8C\x98\x80\x85\x9A \x8E\x89 \x96\x98\x87 \x92\x8C\x89\x84\x8D）"               ,
+    "circleFearsSquares"   : "\x92\x89\x82\x85\x8C \x8E\x94\x87\x83 \x8E\x8E\x98\x85\x81\x92"                                  ,
+    "whoScaresThem"        : "(\x9A'\x96\x98\x89\x8A \x8C\x98\x80\x85\x9A \x82\x8D *\x8E\x8E\x89* \x84\x8D \x8E\x94\x87\x83\x89\x8D)"         ,
+     "areTheyScared"        : "(\x9A'\x96\x98\x89\x8A \x8C\x9A\x94\x85\x91'\x9A\x8D *\x8E\x94\x87\x83\x89\x8D* \x8E\x8E\x98\x85\x81\x92)"       , 
+     "squaresSnubCircles"   : "\x8E\x98\x85\x81\x92\x89\x8D \x8E\x83\x87\x89\x97\x89\x8D \x92\x89\x82\x85\x8C\x89\x8D!!"                                  ,
+    "areTheySnubbed"       : "(\x9A'\x96\x98\x89\x8A \x8C\x9A\x94\x85\x91'\x9A\x8D *\x9A\x85\x8A \x8B\x83\x89* \x99\x84\x8D \x8E\x83\x87\x89\x97\x89\x8D \x92\x89\x82\x85\x8C)"        , 
+    "everyoneHates"        : "\x8B\x85\x8C\x8D \x99\x85\x90\x80\x89\x8D \x80\x9A \x8B\x85\x8C\x8D!!1!"                           ,
+   "almostEveryoneHates"  : "\x8B\x8E\x92\x88 \x8B\x85\x8C\x8D \x99\x85\x90\x80\x89\x8D \x80\x9A \x8B\x85\x8C\x8D..."                     ,
+   "squaresHateCircles"   : "\x8E\x98\x85\x81\x92\x89\x8D \x99\x85\x90\x80\x89\x8D \x92\x89\x82\x85\x8C\x89\x8D!!"                                  ,
+   "circlesHateSquares"   : "\x92\x89\x82\x85\x8C\x89\x8D \x99\x85\x90\x80\x89\x8D \x8E\x98\x85\x81\x92\x89\x8D!!"                                  ,
+   "areTheyYelling"       : "(\x9A'\x96\x98\x89\x8A \x8C\x9A\x94\x85\x91'\x9A\x8D *\x96\x85\x98\x87\x89\x8D* \x92\x8C \x80\x87\x98\x89\x8D)"              , 
+    "nerdsNow"             : "\x8E\x84 \x84\x87\x90\x90\x85\x9A \x84\x80\x8C\x84 \x92\x85\x99\x89\x8D \x92\x8B\x99\x89\x85"                        ,
+   "schockedPeep"         : "\x8C\x8E\x84 \x84\x80\x89\x99 \x84\x86\x84 \x81\x84\x8C\x8D?"                              ,
+     "whatever"             : "\x90\x92\x92.. \x99\x89\x84\x89\x84"                                              ,
     
    // Act 3
     "ellipsis"             : ". . ."                                                 ,
-    "coolNoMore"           : "כובעים זה כבר לא מגניב, מה לא שמעת?"           ,
-    "beScared"             : "תפחדו. תכעסו."                                  ,
+    "coolNoMore"           : "\x8B\x85\x81\x92\x89\x8D \x86\x84 \x8B\x81\x98 \x8C\x80 \x8E\x82\x90\x89\x81, \x8E\x84 \x8C\x80 \x99\x8E\x92\x9A?"           ,
+    "beScared"             : "\x9A\x94\x87\x83\x85. \x9A\x8B\x92\x91\x85."                                  ,
     
     // MANIFESTO
     "manifesto" : [
        //"as if you viewers want GOOD news"       ,
-      "מי ירצה לראות *אנשים מסתדרים*",
-        "שלום זה משעמם. אלימות זה ויראלי"    ,
+      "\x8E\x89 \x89\x98\x96\x84 \x8C\x98\x80\x85\x9A *\x80\x90\x99\x89\x8D \x8E\x91\x9A\x83\x98\x89\x8D*",
+        "\x99\x8C\x85\x8D \x86\x84 \x8E\x99\x92\x8E\x8D. \x80\x8C\x89\x8E\x85\x9A \x86\x84 \x85\x89\x98\x80\x8C\x89"    ,
         //"peace is boring. conflict gets clicks." ,
-        "וכל סיפור צריך קונפליקט, אז..."  ,
+        "\x85\x8B\x8C \x91\x89\x94\x85\x98 \x96\x98\x89\x8A \x97\x85\x90\x94\x8C\x89\x97\x88, \x80\x86..."  ,
         //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
-        "תנו לקהל את מה שהם רוצים."
+        "\x9A\x90\x85 \x8C\x97\x84\x8C \x80\x9A \x8E\x84 \x99\x84\x8D \x98\x85\x96\x89\x8D."
      ] ,
      
      // -- Strings currently in graphics resources --
         // (bg_preload.png) Preload screen
-     "playingTime"        : "זמן משחק: בערך 5 דקות" ,
-     "warning"            : 'תורגם לעברית ע"י איתן בילצקי (טנא)' ,
+     "playingTime"        : "\x86\x8E\x8F \x8E\x99\x87\x97: \x81\x92\x98\x8A 5 \x83\x97\x85\x9A" ,
+     "warning"            : '\x9A\x85\x98\x82\x8D \x8C\x92\x81\x98\x89\x9A \x92"\x89 \x80\x89\x9A\x8F \x81\x89\x8C\x96\x97\x89 (\x88\x90\x80)' ,
      
      // (cam-instructions.png) Camera Instructions
-     "pointAndClick"      : "תצביעו & תלחצו" ,
+     "pointAndClick"      : "\x9A\x96\x81\x89\x92\x85 & \x9A\x8C\x87\x96\x85" ,
      
      // (chyron3.png)
-     "chyronNothing"      : "אה-יוו, זה *כלום*" ,
+     "chyronNothing"      : "\x80\x84-\x89\x85\x85, \x86\x84 *\x8B\x8C\x85\x8D*" ,
      
      // (credits0001.png)
-     "createdBy"          : 'תורגם לעברית ע"י איתן בילצקי (טנא)' ,
-     "NickyCase"          : 'התכנית המוקרית ע"י Nicky Case' ,
+     "createdBy"          : '\x9A\x85\x98\x82\x8D \x8C\x92\x81\x98\x89\x9A \x92"\x89 \x80\x89\x9A\x8F \x81\x89\x8C\x96\x97\x89 (\x88\x90\x80)' ,
+     "NickyCase"          : '\x84\x9A\x8B\x90\x89\x9A \x84\x8E\x85\x97\x98\x89\x9A \x92"\x89 Nicky Case' ,
      
      // (credits0002.png)
-     "manyThanks"         : "תודה רבה לפלייטסטרים שלי:" ,
+     "manyThanks"         : "\x9A\x85\x83\x84 \x98\x81\x84 \x8C\x94\x8C\x89\x89\x88\x91\x88\x98\x89\x8D \x99\x8C\x89:" ,
      
      // (credits000[3,4,5,6].png)
-     "patreonSupporters"  : "לתומכים שלי בפטריאון:" ,
+     "patreonSupporters"  : "\x8C\x9A\x85\x8E\x8B\x89\x8D \x99\x8C\x89 \x81\x94\x88\x98\x89\x80\x85\x8F:" ,
      
      // (credits0007.png)
-     "lastButNotLeast"    : "ואחרונים חביבים," ,
+     "lastButNotLeast"    : "\x85\x80\x87\x98\x85\x90\x89\x8D \x87\x81\x89\x81\x89\x8D," ,
      
      // (credits0008.png)
-     "thankYouForPlaying" : "תודה *לכם* ששיחקתם!" ,
+     "thankYouForPlaying" : "\x9A\x85\x83\x84 *\x8C\x8B\x8D* \x99\x99\x89\x87\x97\x9A\x8D!" ,
      
      // (preload_play.png)
-     "playButton"         : "יאללה למשחק" ,
+     "playButton"         : "\x89\x80\x8C\x8C\x84 \x8C\x8E\x99\x87\x97" ,
      
      // (end_button.png) Post Credits
-     "otherWorkButton"    : "לעמוד של ניקי" ,
-     "buyCoffeeButton"    : "לקנות לי קפה" ,
-     "replayButton"       : "שחקו שוב בבלגן הזה" ,
+     "otherWorkButton"    : "\x8C\x92\x8E\x85\x83 \x99\x8C \x90\x89\x97\x89" ,
+     "buyCoffeeButton"    : "\x8C\x97\x90\x85\x9A \x8C\x89 \x97\x94\x84" ,
+     "replayButton"       : "\x99\x87\x97\x85 \x99\x85\x81 \x81\x81\x8C\x82\x8F \x84\x86\x84" ,
      
      // (logo.png) Post Credits
-     "logoWBWWB"          : "שתפו את הכאב:" ,
+     "logoWBWWB"          : "\x99\x9A\x94\x85 \x80\x9A \x84\x8B\x80\x81:" ,
      
      // (end_prototype.png)
-     "endOfPrototype"     : "סיימנו ת'משחק! רגע.." ,
-     "toBeContinued"      : "(המשך יבוא)" ,
+     "endOfPrototype"     : "\x91\x89\x89\x8E\x90\x85 \x9A'\x8E\x99\x87\x97! \x98\x82\x92.." ,
+     "toBeContinued"      : "(\x84\x8E\x99\x8A \x89\x81\x85\x80)" ,
      
      // (quote0002.png)
      "WBWWB"              : "我们变成了我们所看到的." ,
